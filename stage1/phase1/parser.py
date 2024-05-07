93ccddd28dc889fc77b97f11555ce119ed81f74c
Mark Joshwel <mark@joshwel.co>
2024-05-07 20:32:31 +0000

    s1p1a: groundwork

--- stage1/phase1/parser.py
+++ stage1/phase1/parser.py
@@ -28,3 +28,174 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 For more information, please refer to <http://unlicense.org/>
 """
+
+from enum import Enum
+from typing import NamedTuple
+
+
+class TyuUnitEnum(Enum):
+    # unit    : comment
+    #         | assignment
+    #         | include
+    #         | control
+    #         | declaration
+    #         | expression
+    #         ;
+    WHITESPACE = 1
+    COMMENT = 2
+    ASSIGNMENT = 3
+    INCLUDE = 4
+    CONTROL = 5
+    CONTROL_IF = 6
+    CONTROL_ELSE = 7
+    CONTROL_CALL = 8
+    CONTROL_RETURN = 9
+    DECLARATION = 10
+    DECLARATION_SCOPE = 11
+    DECLARATION_REGISTER = 12
+    EXPRESSION = 13
+
+    def __repr__(self) -> str:  # reimpl not needed
+        return f"{self.__class__.__name__}.{self.name}"
+
+
+class TyuSubunitEnum(Enum):
+    NUMBER = 1
+    CHARACTER = 2
+    VALUE = 3
+    REGISTER = 4
+    NAME = 5
+    SCOPE_INNARD = 6
+    TYPE = 7
+
+    def __repr__(self) -> str:  # reimpl not needed
+        return f"{self.__class__.__name__}.{self.name}"
+
+
+class OneOrMore(NamedTuple):
+    unit: TyuUnitEnum | TyuSubunitEnum
+
+    def __repr__(self) -> str:  # reimpl not needed
+        return f"{self.unit}+"
+
+
+class ZeroOrMore(NamedTuple):
+    unit: TyuUnitEnum | TyuSubunitEnum
+
+    def __repr__(self) -> str:  # reimpl not needed
+        return f"{self.unit}*"
+
+
+TyuSubUnitPrimitiveType = TyuUnitEnum | TyuSubunitEnum | OneOrMore | ZeroOrMore | str
+TyuSubunitType = TyuSubUnitPrimitiveType | list[TyuSubUnitPrimitiveType]
+
+TyuSyntax: dict[TyuUnitEnum, tuple[TyuSubunitType, ...]] = {
+    # control : if | else | call | return ;
+    # if      : "?"  ( register | value | decl_scope )        decl_scope ;
+    # else    : ","  ( register | value | decl_scope | "_" )  decl_scope ;
+    # call    : "@"  ( register | decl_scope | "!" )          ( register | "_" ) ;
+    # return  : "`"  ( register | value | decl_scope )        "`" ;
+    TyuUnitEnum.CONTROL: (
+        [
+            TyuUnitEnum.CONTROL_IF,
+            TyuUnitEnum.CONTROL_ELSE,
+            TyuUnitEnum.CONTROL_CALL,
+            TyuUnitEnum.CONTROL_RETURN,
+        ],
+    ),
+    TyuUnitEnum.CONTROL_IF: (
+        "?",
+        [TyuSubunitEnum.REGISTER, TyuSubunitEnum.VALUE, TyuUnitEnum.DECLARATION_SCOPE],
+        TyuUnitEnum.DECLARATION_SCOPE,
+    ),
+    TyuUnitEnum.CONTROL_ELSE: (
+        "?",
+        [
+            TyuSubunitEnum.REGISTER,
+            TyuSubunitEnum.VALUE,
+            TyuUnitEnum.DECLARATION_SCOPE,
+            "_",
+        ],
+        TyuUnitEnum.DECLARATION_SCOPE,
+    ),
+    TyuUnitEnum.CONTROL_CALL: (
+        "@",
+        [
+            TyuSubunitEnum.REGISTER,
+            TyuUnitEnum.DECLARATION_SCOPE,
+            "!",
+        ],
+        TyuUnitEnum.DECLARATION_SCOPE,
+    ),
+    TyuUnitEnum.CONTROL_RETURN: (
+        "`",
+        [TyuSubunitEnum.REGISTER, TyuSubunitEnum.VALUE, TyuUnitEnum.DECLARATION_SCOPE],
+        "`",
+    ),
+    # declaration   : decl_scope | decl_register ;
+    # decl_scope    : "("  unit*     ")"  ;
+    # decl_register : "d"  name      type ;
+    TyuUnitEnum.DECLARATION: (
+        [TyuUnitEnum.DECLARATION_SCOPE, TyuUnitEnum.DECLARATION_REGISTER],
+    ),
+    TyuUnitEnum.DECLARATION_SCOPE: ("(", TyuSubunitEnum.SCOPE_INNARD, ")"),
+    TyuUnitEnum.DECLARATION_REGISTER: (
+        "d",
+        TyuSubunitEnum.NAME,
+        TyuSubunitEnum.TYPE,
+    ),
+    # assignment : ":"  register  ( register | value | decl_scope ) ;
+    # include    : "#"  CHARACTER+  "~" ;
+    # comment    : ";"  CHARACTER*  ( "\n" | ";") ;
+    TyuUnitEnum.ASSIGNMENT: (
+        ":",
+        TyuSubunitEnum.REGISTER,
+        [
+            TyuSubunitEnum.REGISTER,
+            TyuSubunitEnum.VALUE,
+            TyuUnitEnum.DECLARATION_SCOPE,
+        ],
+    ),
+    TyuUnitEnum.INCLUDE: (
+        "#",
+        OneOrMore(TyuSubunitEnum.CHARACTER),
+    ),
+    TyuUnitEnum.COMMENT: (
+        ";",
+        ZeroOrMore(TyuSubunitEnum.CHARACTER),
+        ["\n", ";"],
+    ),
+    # expression : ( "+" | "-" | "*" | "/" | "%" | "l" | "r"
+    # .              | "=" | "<" | ">" | "[" | "]" | "c"
+    # .              | "&" | "|" | "!" | "^" | "7" | "\\" | "1" | "6" )
+    # .            ( register | value )
+    # .            ( register | value );
+    TyuUnitEnum.EXPRESSION: (
+        [
+            "+",
+            "-",
+            "*",
+            "/",
+            "%",
+            "l",
+            "r",
+            "=",
+            "<",
+            ">",
+            "[",
+            "]",
+            "c",
+            "&",
+            "|",
+            "!",
+            "^",
+            "7",
+            "\\",
+            "1",
+            "6",
+            "t",
+        ],
+        [TyuSubunitEnum.REGISTER, TyuSubunitEnum.VALUE],
+        [TyuSubunitEnum.REGISTER, TyuSubunitEnum.VALUE],
+    ),
+}
